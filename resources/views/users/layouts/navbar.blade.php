1ed33530d1b049c5edc1421e11df6beb3025963b
The Gardener 69 <gether@hybrain.co>
2017-12-29 11:02:53 +0800

    Updates

--- resources/views/users/layouts/navbar.blade.php
+++ resources/views/users/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top custom-navbar" {{-- style="background-color: rgba(229, 202, 159, 1) !important;" --}}>
     <div class="container">
         <a class="navbar-brand text-white" href="#">
-            <img src="{{ asset('storage/anp-logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
+            <img src="{{ asset('storage/anp-logo-white-2.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
             {{ config('app.name') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
